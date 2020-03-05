b9ae4ec0b2305ea3a62f96b9537ca0d6b64dc820
gitLFFF <33696594+gitLFFF@users.noreply.github.com>
2020-03-05 17:29:52 +0800

    Update controller.java

--- src/main/java/com/lf/some/controller.java
+++ src/main/java/com/lf/some/controller.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class controller {
     @GetMapping("/index")
     public String getIndex(){
-        
+        System.out.println("他人提交");
         //冲突
         return "helloword";
     }
