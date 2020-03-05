b2150c12fb4260d5e2c2b6d6abac98328fcf7b95
gitLFFF <%&@90046109lf>
2020-03-05 18:09:09 +0800

    我刚刚提交

--- src/main/java/com/lf/some/controller.java
+++ src/main/java/com/lf/some/controller.java
@@ -7,9 +7,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class controller {
     @GetMapping("/index")
     public String getIndex(){
-        System.out.println("他人提交");
+        System.out.println("最近冲突解决流程");
         //冲突
         return "helloword";
     }
-   
+    @GetMapping("/index1")
+    public String getIndex1(){
+
+        return "helloword";
+    }
 }
